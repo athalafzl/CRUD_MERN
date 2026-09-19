d5adfb1d435899c01a2319e94ce9cc78a319b038
athalafzl <athalafazli6@gmail.com>
2026-09-19 11:08:45 +0700

    update

--- frontend/src/AuthProvider.jsx
+++ frontend/src/AuthProvider.jsx
@@ -41,16 +41,19 @@ function AuthProvider({ children }) {
 
   async function login(email, password) {
     try {
-      const response = await fetch("http://localhost:3000/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+      const response = await fetch(
+        "https://crud-mern-15iw.vercel.app/users/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
         },
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-      });
+      );
       const data = await response.json();
       if (!response.ok) {
         return data.message;
